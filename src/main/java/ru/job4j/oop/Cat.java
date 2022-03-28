defdab838afc2ffc5498af1fafd0abfa984a5c24
aluselex <asaleksandrova@mail.ru>
2022-03-28 20:24:56 +0300

    Cat

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,7 +1,8 @@
 package ru.job4j.oop;
 
 public class Cat {
-    Cat perry = new Cat();
-    Cat sparky = new Cat();
-
+    public static void main(String[] args) {
+        Cat perry = new Cat();
+        Cat sparky = new Cat();
+    }
 }
