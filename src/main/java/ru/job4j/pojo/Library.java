5e5e692dd994527bb6da630a9a613455fee11ea4
aluselex <asaleksandrova@mail.ru>
2022-05-05 20:15:54 +0300

    3. Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,35 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book zero = new Book("Dandelion wine", 318);
+        Book first = new Book("Platero y yo", 158);
+        Book second = new Book("Clear code", 100);
+        Book third = new Book("The lost world", 320);
+        Book[] books = new Book[4];
+        books[0] = zero;
+        books[1] = first;
+        books[2] = second;
+        books[3] = third;
+        for (int i = 0; i < books.length; i++) {
+            Book book = books[i];
+            System.out.println(book.getName() + " - " + book.getPages());
+        }
+        System.out.println("Replace books 0 and 3:");
+        Book temp = books[0];
+        books[0] = books[3];
+        books[3] = temp;
+        for (int i = 0; i < books.length; i++) {
+            Book book = books[i];
+            System.out.println(book.getName() + " - " + book.getPages());
+        }
+        System.out.println("Clear code book:");
+        for (int i = 0; i < books.length; i++) {
+            Book book = books[i];
+            if (book.getName().equals("Clear code")) {
+                System.out.println(book.getName() + " - " + book.getPages());
+            }
+
+        }
+    }
+}
