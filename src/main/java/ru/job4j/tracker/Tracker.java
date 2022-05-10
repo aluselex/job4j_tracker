a889e5e7ce4bdf53512c90a1b736a8a650afb84c
aluselex <asaleksandrova@mail.ru>
2022-05-10 16:36:55 +0300

    5. Tracker - хранилище [#396 #299118]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -27,19 +27,11 @@ public class Tracker {
     }
 
     public Item[] findAll() {
-        Item[] result = new Item[size];
-        int count = 0;
-        for (int i = 0; i < size; i++) {
-            if (items[i] != null) {
-            result[count] = items[i];
-            count++;
-            }
-        }
-        return Arrays.copyOf(result, count);
+        return Arrays.copyOf(items, size);
     }
 
         public Item[] findByName(String key) {
-            Item[] result = new Item[items.length];
+            Item[] result = new Item[size];
             int count = 0;
             for (int i = 0; i < size; i++) {
                 Item item = items[i];
