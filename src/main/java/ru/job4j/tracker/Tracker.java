3069c25eccfdc716cc9a6d8a2718b64b0900f785
aluselex <asaleksandrova@mail.ru>
2022-06-03 22:44:03 +0300

    6. Метод замены заявки. Tracker.replace [#211748]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -14,33 +14,46 @@ public class Tracker {
         return item;
     }
 
-    public Item findById(int id) {
-        Item rsl = null;
+    private int indexOf(int id) {
+        int rsl = -1;
         for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
+            if (items[index].getId() == id) {
+                rsl = index;
                 break;
             }
         }
         return rsl;
     }
 
+    public Item findById(int id) {
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
+    }
+
     public Item[] findAll() {
         return Arrays.copyOf(items, size);
     }
 
-        public Item[] findByName(String key) {
-            Item[] result = new Item[size];
-            int count = 0;
-            for (int i = 0; i < size; i++) {
-                Item item = items[i];
-                if (key.equals(item.getName())) {
-                    result[count] = items[i];
-                    count++;
-                }
+    public Item[] findByName(String key) {
+        Item[] result = new Item[size];
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            Item item = items[i];
+            if (key.equals(item.getName())) {
+                result[count] = items[i];
+                count++;
             }
-            return Arrays.copyOf(result, count);
         }
+        return Arrays.copyOf(result, count);
     }
 
+    public boolean replace(int id, Item item) {
+        boolean rsl = false;
+        int index = indexOf(id);
+        if (index != -1) {
+            items[index].setName(item.getName());
+            rsl = true;
+        }
+        return rsl;
+    }
+}
